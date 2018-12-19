e37e6bc9a4a9315127fbe8dea617c53b8f5530ce
Alecs <goluzov_a@mail.ru>
2018-12-19 23:00:56 +0300

    HomeWork2

---
+++ HomeWork2/src/main/java/ru/goluzov/se/exception/CustomException.java
@@ -0,0 +1,8 @@
+package ru.goluzov.se.exception;
+
+public class CustomException extends Exception {
+
+    public CustomException(String message) {
+        super(message);
+    }
+}
---
+++ HomeWork2/src/main/java/ru/goluzov/se/exception/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package ru.goluzov.se.exception;
+
+public class MyArraySizeException extends CustomException {
+
+    public MyArraySizeException() {
+        super("The matrix must be of size 4x4");
+    }
+}
